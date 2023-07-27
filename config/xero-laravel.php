11d9f2c4fa2cdacae273077804444cebab41a52a
Hoang Ho <hoang.nhi.ho.1991@gmail.com>
2023-07-27 18:24:24 +1000

    bug fixes

--- config/xero-laravel.php
+++ config/xero-laravel.php
@@ -16,7 +16,7 @@ return [
             'client_id'     => env('XERO_CLIENT_ID'),
             'client_secret' => env('XERO_CLIENT_SECRET'),
             'redirect_uri'  => env('XERO_REDIRECT_URI'),
-            'scope'         => 'openid email profile offline_access accounting.settings.read',
+            'scope'         => 'openid profile email accounting.transactions accounting.contacts offline_access accounting.settings',
         ],
         'invoice_model' => \App\Models\Invoice::class, // Change this to your Invoice model
         'invoice_line_item_model' => \App\Models\InvoiceLineItem::class, // Change this to your Invoice Line Item model
